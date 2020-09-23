3514c421523d62d1a162f654de9ebb333366588b
ross thompson <rossthompson89@gmail.com>
2020-09-23 10:34:30 -0600

    Finished building out chrome automation

---
+++ README.md
@@ -0,0 +1,11 @@
+# amazon product finder
+Selenium driven, python based script that will prompt user for a product to search for, and then find the highest average rated product from said search. Once the product is found it will then be added to the cart. This is designed to work with Chrome, Firefox and Safari. 
+
+** Script workflow **
+- Prompt user for product search
+- Open up web browser(s)
+- Navigate to amazon
+- Log in
+- Search for product
+- Adjust filter
+- Add item to cart
--- amazon_product_finder.py
+++ amazon_product_finder.py
@@ -8,32 +8,24 @@ to the cart. Designed for use with Google Chrome, Firefox and Safari.
 """
 
 # Task list 
-# TODO: Build out Chrome class
 # TODO: Build out Firefox class
 # TODO: Build out Safari class
 # TODO: Build out test framework with pytest
-# TODO: Make sure rogue chrome processes are killed
+# TODO: Make sure rogue chrome processes are killed on host machine
 # TODO: Possibly remove self prefix from web driver variable(s)
-# TODO: combine search and find key lines?
-# TODO: combine all of the class methods into one that does everything
-
-#   Script workflow
-# - Prompt user for product search
-# - Open up web browser(s)
-# - Navigate to amazon
-# - Log in
-# - Search for product
-# - Adjust filter
-# - Add item to cart
+# TODO: Split up classes into different modules?
+# TODO: Convert user login to xpath
 
 import os
-import requests   # might not need this?
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 from time import sleep
 import config
 
 def userPrompt():
-    """Prompt user to search for a product. Return product."""
+    """Prompt user to search for a product. Return product"""
     print("=" * 28, "AMAZON PRODUCT FINDER" ,"=" * 28)
     print("Please input a product to search for, and amazon product finder "
         "will automagically\nfind the highest rated product of your search "
@@ -42,20 +34,28 @@ def userPrompt():
     product_search = input("Product search: ")
     return product_search
 
-class AmazonSearchAndFindProductForChrome():
+class ChromeAmazonProductFinder():
     def __init__ (self, product_search):
-        """Create Selenium driver object and open up Amazon.com on Chrome"""
+        """
+        Create Selenium driver object and open up Amazon.com on Chrome
+        Execute class methods to search for, find and add the product to cart
+        """
         self.product_search = product_search
         options = webdriver.ChromeOptions()
         options.add_experimental_option("detach", True)
         self.chrome_driver = webdriver.Chrome(chrome_options=options)
         self.chrome_driver.get('https://amazon.com')
-        sleep(1)        
+        sleep(3)
+        self.searchForProduct()
+        self.adjustSearchFilter()
+        self.goToProductPage()
+        self.addProductToCart()
+        self.goToCart()
 
     def userLogin(self):
         """
         Log into amazon. This method is temporarily deprecated as logging in
-        via Selenium requires granting authentication permission via email.
+        via Selenium requires granting authentication permission via email
         """
         hello_sign_in_button = self.chrome_driver.find_element_by_css_selector(
             "#nav-link-accountList").click()
@@ -70,35 +70,49 @@ class AmazonSearchAndFindProductForChrome():
         sleep(1)            
 
     def searchForProduct(self):
-        """Search for the product that was specified in the user prompt."""
-        search_bar = self.chrome_driver.find_element_by_id("twotabsearchtextbox")
-        search_bar.send_keys(self.product_search)
-        search_button = self.chrome_driver.find_element_by_id(
-            "nav-search-submit-text").click()
-        sleep(1)
+        """Search for the product that was specified in the user prompt"""
+        search_bar = self.chrome_driver.find_element(
+            By.XPATH, "//*[@id='twotabsearchtextbox']").send_keys(
+            self.product_search, Keys.ENTER)
+        self.chrome_driver.implicitly_wait(5)
 
     def adjustSearchFilter(self):
-        adjust_filter = self.chrome.find_element_by_css_selector(
-            "#a-autoid-0-announce > span.a-dropdown-prompt").click()
-        sort_by_rating = self.chrome.find_element_by_css_selector(
-            "#s-result-sort-select_3").click()
-        sleep(1)
+        """Adjust search filter to filter by highest average review"""
+        adjust_filter = self.chrome_driver.find_element(
+            By.XPATH, "//*[@id='a-autoid-0-announce']").click()
+        self.chrome_driver.implicitly_wait(5)
+        sort_by_rating = self.chrome_driver.find_element(
+            By.XPATH, "//*[@id='s-result-sort-select_3']").click()        
 
     def goToProductPage(self):
-        search_result = self.chrome.find_element_by_xpath(
-            "//div[@data-cel-widget='search_search_result']//img").click()
-        sleep(1)
+        """Click on first result for product page"""
+        go_to_product_page = self.chrome_driver.find_element(
+            By.XPATH, "//div[@data-cel-widget='search_result_1']//img").click()
+        self.chrome_driver.implicitly_wait(5)
         
     def addProductToCart(self):
-        pass
+        """Add product to cart and decline extended warranty if present"""
+        add_to_cart = self.chrome_driver.find_element(
+            By.XPATH, "//*[@id='submit.add-to-cart']").click()
+        try:
+            decline_protection_plan = self.chrome_driver.find_element(
+                By.XPATH, "//*[@id='siNoCoverage-announce']")
+            self.chrome_driver.execute_script(
+                "arguments[0].click();", decline_protection_plan)
+        except NoSuchElementException:
+            pass
 
-    def __str__(self):
-        pass
+    def goToCart(self):
+        """Navigate to shopping cart"""
+        go_to_cart = self.chrome_driver.find_element(
+            By.XPATH, "//*[@id='hlb-view-cart-announce']").click()
 
 
-class AmazonSearchAndFindProductForFirefox():
+class FirefoxAmazonProductFinder():
     def __init__ (self, product_search):
         self.product_search = product_search
+        self.firefox_driver = webdriver.Firefox()
+        self.firefox_driver.get('https://amazon.com')
 
     def userLogin(self):
         pass
@@ -119,7 +133,7 @@ class AmazonSearchAndFindProductForFirefox():
         pass
 
 
-class AmazonSearchAndFindProductForSafari():
+class SafariAmazonProductFinder():
     def __init__ (self, product_search):
         self.product_search = product_search        
 
@@ -142,9 +156,8 @@ class AmazonSearchAndFindProductForSafari():
         pass
 
 if __name__ == '__main__':
-    product_search = userPrompt()
-    chrome_browser = AmazonSearchAndFindProductForChrome(product_search)
-    # chrome_browser.userLogin()
-    chrome_browser.searchForProduct()
-    chrome_browser.adjustSearchFilter()
-    chrome_browser.goToProductPage()
+    # product_search = userPrompt()
+    # chrome_browser = ChromeAmazonProductFinder(product_search)
+
+    product_search = 'electric water kettle'
+    firefox_browser = FirefoxAmazonProductFinder(product_search)
